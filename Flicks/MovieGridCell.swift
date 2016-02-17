8591f648d46c32340ebb0988b1886c5141e1f524
Hyun Lim <hlim@lyft.com>
2016-02-17 13:46:40 -0800

    Add Gridview

---
+++ Flicks/MovieGridCell.swift
@@ -0,0 +1,23 @@
+//
+//  MovieGridCell.swift
+//  Flicks
+//
+//  Created by Hyun Lim on 2/17/16.
+//  Copyright © 2016 Lyft. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+    internal weak var movie:Movie? {
+        didSet {
+            if let movie = self.movie {
+                self.posterImageView.setImageWithURL(movie.getPosterURL(Movie.PHOTO_SIZES.SMALL.rawValue))
+            }
+        }
+    }
+    
+}
--- Flicks/MoviesViewController.swift
+++ Flicks/MoviesViewController.swift
@@ -13,6 +13,8 @@ class MoviesViewController: UIViewController {
 
     @IBOutlet weak var moviesTableView: UITableView!
     @IBOutlet weak var errorNotificationView: UIView!
+    @IBOutlet weak var layoutSelector: UISegmentedControl!
+    @IBOutlet weak var moviesCollectionView: UICollectionView!
     
     var flicksData:FlicksData?
     let refreshControl = UIRefreshControl()
@@ -26,6 +28,10 @@ class MoviesViewController: UIViewController {
         self.moviesTableView.delegate = self
         self.moviesTableView.dataSource = self
         
+        self.moviesCollectionView.delegate = self
+        self.moviesCollectionView.dataSource = self
+        self.moviesCollectionView.hidden = true
+        
         self.flicksData = FlicksData()
         self.flicksData!.delegate = self
         
@@ -33,7 +39,7 @@ class MoviesViewController: UIViewController {
         self.moviesTableView.insertSubview(self.refreshControl, atIndex: 0)
         
         self.flicksData!.refetchPosts(self.endpoint!, success: { () -> Void in
-                self.moviesTableView.reloadData()
+                self.reloadView()
             }, error: { (_: (NSError?)) in
             })
         
@@ -50,20 +56,45 @@ class MoviesViewController: UIViewController {
     
     func refreshControlAction(refreshControl: UIRefreshControl) {
         self.flicksData!.refetchPosts(self.endpoint!, success: { () -> Void in
-            self.moviesTableView.reloadData()
+                self.reloadView()
             }, error: { (_: (NSError?)) in
         })
+    }
+    
+    @IBAction func onLayoutSelectorChanged(sender: UISegmentedControl) {
+        let selected = sender.selectedSegmentIndex
+        if selected == 0 {
+            self.moviesTableView.hidden = false
+            self.moviesCollectionView.hidden = true
+        } else {
+            self.moviesTableView.hidden = true
+            self.moviesCollectionView.hidden = false
+        }
+        self.reloadView()
+    }
+    
+    private func getMoviesCount() -> Int {
+        if let flicksData = self.flicksData as FlicksData? {
+            return flicksData.movies.count
+        } else {
+            return 0
+        }
+    }
+    
+    private func reloadView() {
+        if !self.moviesTableView.hidden {
+            self.moviesTableView.reloadData()
+        } else {
+            self.moviesCollectionView.reloadData()
+        }
+        
     }
 }
 
 extension MoviesViewController: UITableViewDataSource {
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if let flicksData = self.flicksData as FlicksData? {
-            return flicksData.movies.count
-        } else {
-            return 0
-        }
+        return self.getMoviesCount()
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
@@ -99,5 +130,28 @@ extension MoviesViewController: FlicksDataDelegateProtocol {
         self.errorNotificationView.hidden = false
         MBProgressHUD.hideHUDForView(self.view, animated: true)
     }
+    
+}
+
+extension MoviesViewController: UICollectionViewDataSource {
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.getMoviesCount()
+    }
+    
+    // The cell that is returned must be retrieved from a call to -dequeueReusableCellWithReuseIdentifier:forIndexPath:
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("com.lyft.movieGridCell", forIndexPath: indexPath) as! MovieGridCell
+        
+        if let flicksData = self.flicksData as FlicksData? {
+            cell.movie = flicksData.movies[indexPath.row]
+        }
+        
+        return cell
+    }
+    
+}
+
+extension MoviesViewController: UICollectionViewDelegate {
     
 }
